a20aa7304def75143ba091d3e4ca09849fe40d43
Enzo Gallo <enzo@footbar.com>
2025-12-05 12:45:57 +0100

    Initial Commit

---
+++ Discover/DiscoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoverApp.swift
+//  Discover
+//
+//  Created by Enzo Gallo on 05/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct DiscoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
